5193c93b2b53d36b186ed84a79a7730bdbf3df5a
Phil Hurvitz <phurvitz@uw.edu>
2022-01-08 13:16:03 -0800

    20220108a

--- files/file_naming.R
+++ files/file_naming.R
@@ -1,33 +1,34 @@
-# make a stupid directory
-stupid_dir <- file.path(Sys.getenv("TEMP"), "delete_me_later")
+# make a dummy directory
+dud_dir <- normalizePath(file.path(tempdir(), "delete_me_later"), winslash = "/")
 
-if(!dir.exists(stupid_dir)){
-    dir.create(path = stupid_dir)
+if(!dir.exists(dud_dir)){
+    dir.create(path = dud_dir)
 }
 
-# move into the directory
-setwd(stupid_dir)
-
-# a data frame to write to a file
+# make a dummy a data frame to write to a file
 x <- data.frame(foo = "bar")
 
 # make some stupidly named files, Dec 16, 2021
-write.csv(x = x, file = file.path(stupid_dir, "file 121621.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file 12_16_21.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file 12-16-21.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file 16-12-21.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file_121621.csv"), quote = FALSE, row.names = FALSE)
+write.csv(x = x, file = file.path(dud_dir, "file 121621.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file 12_16_21.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file 12-16-21.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file 16-12-21.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file_121621.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
 
 # make some stupidly named files, Dec 17, 2021
-write.csv(x = x, file = file.path(stupid_dir, "file 121721.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file 12_17_21.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file 12-17-21.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file 17-12-21.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file_121721.csv"), quote = FALSE, row.names = FALSE)
+write.csv(x = x, file = file.path(dud_dir, "file 121721.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file 12_17_21.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file 12-17-21.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file 17-12-21.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file_121721.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
 
 # write two logically named files
-write.csv(x = x, file = file.path(stupid_dir, "file_20211216.csv"), quote = FALSE, row.names = FALSE)
-write.csv(x = x, file = file.path(stupid_dir, "file_20211217.csv"), quote = FALSE, row.names = FALSE)
+write.csv(x = x, file = file.path(dud_dir, "file_20211217.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file_20211216.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file_2021-12-17.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file_2021-12-16.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file_2021_12_17.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
+write.csv(x = x, file = file.path(dud_dir, "file_2021_12_16.csv"), quote = FALSE, row.names = FALSE); Sys.sleep(1)
 
 message("which file is newer?")
 write.csv(list.files(stupid_dir), quote = FALSE, row.names = FALSE)
